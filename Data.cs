ed23bf69f2fde208822399527054ce227fd5caa2
Saida <122756140+gsaida@users.noreply.github.com>
2023-06-05 23:23:35 -0400

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace StoicQuotes
+{
+	public class Data
+	{
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("quote")]
+        public string Quote { get; set; }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using StoicQuotes;
+
+
+
+public class Program
+{
+    public static async Task Main()
+    {
+
+        HttpClient client = new HttpClient();
+
+
+        HttpResponseMessage response = client.GetAsync("https://api.themotivate365.com/stoic-quote").Result;
+
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonObject = await response.Content.ReadAsStringAsync();
+
+
+            Data data = JsonConvert.DeserializeObject<Data>(jsonObject);
+
+
+            Console.WriteLine($"Author: {data.Author}" + ", " + $"Quote: {data.Quote}");
+
+        }
+        else
+        {
+            Console.WriteLine("Oops. Try again maybe?");
+        }
+    }
+}
